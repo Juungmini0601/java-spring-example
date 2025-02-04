7b87d6efcd33a0b7443a5683fa4f1e69581ce088
김정민 <raon37910@gmail.com>
2025-02-04 14:40:10 +0900

    add spring web example project

---
+++ spring-web-example/src/test/java/com/raon/spring/web/example/SpringWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon.spring.web.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
