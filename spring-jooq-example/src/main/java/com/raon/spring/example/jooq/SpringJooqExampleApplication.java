cc52434b6ae4a1aac3747c02908f3dd97159959c
김정민 <raon37910@gmail.com>
2025-02-07 13:46:18 +0900

    Jooq Example Init

---
+++ spring-jooq-example/src/main/java/com/raon/spring/example/jooq/SpringJooqExampleApplication.java
@@ -0,0 +1,13 @@
+package com.raon.spring.example.jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJooqExampleApplication.class, args);
+	}
+
+}
---
+++ spring-jooq-example/src/test/java/com/raon/spring/example/jooq/SpringJooqExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raon.spring.example.jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJooqExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
