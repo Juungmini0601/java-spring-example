235a3fa8a85c2a43b25de68c68db5871898af539
김정민 <raon37910@gmail.com>
2025-02-24 14:42:35 +0900

    spring example 코드 추가

--- java-example/src/main/java/Main.java
+++ java-example/src/main/java/Main.java
@@ -1,50 +1,58 @@
 import java.util.Scanner;
 
 public class Main {
+
 	public static int n;
+	public static int m;
 	public static int[][] arr;
-	public static int[][] maxDp; // 경로상 최대 값이 최소가 되는 경우
+	public static int[][] dp;
 
 	public static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		input();
+		init();
+		dp[0][0] = 1;
 
-		maxDp[1][1] = arr[1][1];
-
-		// 1열 채우기
-		for (int i = 2; i <= n; i++) {
-			maxDp[1][i] = Math.max(maxDp[1][i - 1], arr[1][i]);
-		}
-
-		// 2열부터 끝까지 채우기
-		for (int i = 2; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (j == 1) {
-					maxDp[i][j] = Math.max(maxDp[i - 1][j], arr[i][j]);
-					continue;
-				}
-
-				// 최대값 고르기
-				maxDp[i][j] = arr[i][j];
-				// 이전 최대 값 중 최소 값 선택
-				int temp = Math.min(maxDp[i][j - 1], maxDp[i - 1][j]);
-				maxDp[i][j] = Math.max(maxDp[i][j], temp);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				find(i, j);
 			}
 		}
 
-		System.out.println(maxDp[n][n]);
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				ans = Math.max(ans, dp[i][j]);
+			}
+		}
 
+		System.out.println(ans);
 	}
 
-	public static void input() {
-		n = sc.nextInt();
-		arr = new int[n + 1][n + 1];
-		maxDp = new int[n + 1][n + 1];
+	public static void find(int row, int col) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (dp[i][j] == Integer.MIN_VALUE) {
+					continue;
+				}
 
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
+				if (arr[i][j] < arr[row][col]) {
+					dp[row][col] = Math.max(dp[row][col], dp[i][j] + 1);
+				}
+			}
+		}
+	}
+
+	public static void init() {
+		n = sc.nextInt();
+		m = sc.nextInt();
+		arr = new int[n][m];
+		dp = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				arr[i][j] = sc.nextInt();
+				dp[i][j] = Integer.MIN_VALUE;
 			}
 		}
 	}
